db568d151c9282b4870e487da1a1b9bd30b606a8
Jéssica Amorim <jessicamorim.ti@gmail.com>
2023-10-30 20:56:15 -0300

    Aula 15

--- curso-java-basico/src/com/loiane/cursojava/aula15/Turno.java
+++ curso-java-basico/src/com/loiane/cursojava/aula15/Turno.java
@@ -8,8 +8,17 @@ public class Turno {
 		
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Turno (m - v - n): ");
+		String turno = scan.next();
 		
-
+			switch(turno) {
+				case "m":
+				case "M": System.out.println("Bom dia!"); break;
+				case "v":
+				case "V": System.out.println("Boa Tarde!"); break;
+				case "n":
+				case "N": System.out.println("Boa Noite!"); break;	
+				default: System.out.println("Valor invalido");	
+			}
 	}
-
 }
